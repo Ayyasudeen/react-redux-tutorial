d64452f58f5bc6bb992b487e96ec5dcbc72b216e
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2022-03-19 19:46:05 +0530

    Reducer Refactoring

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,16 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
     articles: []
 };
 
 function rootReducer(state = initialState, action) {
+    if (action.type === ADD_ARTICLE) {
+        // state.articles.push(action.payload)  <--- This method is wrong. It breaks the immutability principle 
+        return Object.assign({}, state, {
+            articles: state.articles.concat(action.payload)
+        }); 
+    }
     return state;
 }
 
